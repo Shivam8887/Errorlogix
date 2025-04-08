c630a0df1d9006f2465fd6389a0d822e0fe65e2d
Shivam Pandey <156385069+Shivam8887@users.noreply.github.com>
2025-04-08 10:11:20 +0530

    Update api.js

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -1,3 +1,4 @@
-const host = `http://localhost:3000`;
+// const host = `http://localhost:3000`;
+const host = `https://errorlogix2.onrender.com`;
 export const errorapi = `${host}/error`
 export const codeapi = `${host}/codechecker`
